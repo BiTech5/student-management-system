3b05d07073dd1fcf2060a3c061523fd7a7206c02
BiTech5 <bikikc7@gmail.com>
2024-12-28 14:50:10 +0545

    new upadte of code

--- students/views.py
+++ students/views.py
@@ -1,28 +1,27 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse ,HttpRequest
-# Create your views here.
-
+from django.http import HttpResponse, HttpRequest
 from .models import Student
+from typing import Union, Optional
 
 def index(request: HttpRequest) -> HttpResponse:
-    students_data = Student.objects.all()  # Fetch all student records from the database
+    students_data: list[Student] = Student.objects.all()  # Fetch all student records from the database
     return render(request, 'home.html', {'students': students_data})
 
-def add_student(request:HttpRequest)->HttpResponse:
-    if request.method=="POST":
-        first_name = request.POST.get('first_name')
-        last_name = request.POST.get('last_name')
-        roll_number = request.POST.get('roll_number')
-        gender = request.POST.get('gender')
-        date_of_birth = request.POST.get('date_of_birth')
-        email = request.POST.get('email')
-        phone_number = request.POST.get('phone_number')
-        address = request.POST.get('address')
-        grade_level = request.POST.get('grade_level')
-        parent_name = request.POST.get('parent_name')
-        parent_phone = request.POST.get('parent_phone')
-        blood_group = request.POST.get('blood_group')
-        photo = request.FILES.get('photo')
+def add_student(request: HttpRequest) -> HttpResponse:
+    if request.method == "POST":
+        first_name: str = request.POST.get('first_name', '')
+        last_name: str = request.POST.get('last_name', '')
+        roll_number: int = int(request.POST.get('roll_number', 0))
+        gender: str = request.POST.get('gender', '')
+        date_of_birth: str = request.POST.get('date_of_birth', '')
+        email: str = request.POST.get('email', '')
+        phone_number: str = request.POST.get('phone_number', '')
+        address: str = request.POST.get('address', '')
+        grade_level: int = int(request.POST.get('grade_level', 0))
+        parent_name: str = request.POST.get('parent_name', '')
+        parent_phone: str = request.POST.get('parent_phone', '')
+        blood_group: str = request.POST.get('blood_group', '')
+        photo: Optional[Union[bytes, str]] = request.FILES.get('photo')
 
         # Create a new Student instance and save it to the database
         new_student = Student(
@@ -42,42 +41,38 @@ def add_student(request:HttpRequest)->HttpResponse:
         )
         new_student.save()
         return redirect('index')
-    return render(request,'add_student.html')
+    return render(request, 'add_student.html')
 
-
-def view_detail(request:HttpRequest,id:int)->HttpResponse:
-    student = Student.objects.get(id=id)  
+def view_detail(request: HttpRequest, id: int) -> HttpResponse:
+    student: Student = Student.objects.get(id=id)  
     return render(request, 'view_detail.html', {'student': student})
-from datetime import datetime
-import os
-def edit_student(request: HttpRequest, id: int) -> HttpResponse:
-    student = Student.objects.get(id=id)
 
-    dob=student.date_of_birth.strftime("%Y-%m-%d")
+def edit_student(request: HttpRequest, id: int) -> HttpResponse:
+    student: Student = Student.objects.get(id=id)
+    dob: str = student.date_of_birth.strftime("%Y-%m-%d")
+    
     if request.method == "POST":
-        student.first_name = request.POST.get('first_name')
-        student.last_name = request.POST.get('last_name')
-        student.roll_number = request.POST.get('roll_number')
-        student.gender = request.POST.get('gender')
-        student.date_of_birth = request.POST.get('date_of_birth')
-        student.email = request.POST.get('email')
-        student.phone_number = request.POST.get('phone_number')
-        student.address = request.POST.get('address')
-        student.grade_level = request.POST.get('grade_level')
-        student.parent_name = request.POST.get('parent_name')
-        student.parent_phone = request.POST.get('parent_phone')
-        student.blood_group = request.POST.get('blood_group')
+        student.first_name = request.POST.get('first_name', '')
+        student.last_name = request.POST.get('last_name', '')
+        student.roll_number = int(request.POST.get('roll_number', 0))
+        student.gender = request.POST.get('gender', '')
+        student.date_of_birth = request.POST.get('date_of_birth', '')
+        student.email = request.POST.get('email', '')
+        student.phone_number = request.POST.get('phone_number', '')
+        student.address = request.POST.get('address', '')
+        student.grade_level = int(request.POST.get('grade_level', 0))
+        student.parent_name = request.POST.get('parent_name', '')
+        student.parent_phone = request.POST.get('parent_phone', '')
+        student.blood_group = request.POST.get('blood_group', '')
         
         if 'photo' in request.FILES:
-            # print("Photo uploaded:", request.FILES['photo'])
-            student.photo=request.FILES['photo']        
+            student.photo = request.FILES['photo']        
         student.save()
-        # print("Updated photo path:", student.photo.url)
         return redirect('detail', id=student.id)
     
-    return render(request, 'edit.html', {'student': student,'dob':dob})
+    return render(request, 'edit.html', {'student': student, 'dob': dob})
 
-def delete_data(request:HttpRequest,id:int)->HttpRequest:
-    student=Student.objects.get(id=id)
+def delete_data(request: HttpRequest, id: int) -> HttpResponse:
+    student: Student = Student.objects.get(id=id)
     student.delete()
     return redirect('index')
