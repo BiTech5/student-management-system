0991019e33f4a6a4b7d87f7485780fe5c9188ff5
BiTech5 <bikikc7@gmail.com>
2024-12-28 10:45:06 +0545

    upload dynamic image

--- students/views.py
+++ students/views.py
@@ -2,5 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse ,HttpRequest
 # Create your views here.
 
-def index(request:HttpRequest)->HttpResponse:
-    return render(request,'home.html')
+from .models import Student
+
+def index(request: HttpRequest) -> HttpResponse:
+    students_data = Student.objects.all()  # Fetch all student records from the database
+    return render(request, 'home.html', {'students': students_data})
