0ba3a34abe9231c131cfd118cbeb9f4c24bcae44
Bikram <bikikc7@gmail.com>
2025-07-29 08:49:30 +0545

    creating common abstract class for future inheritance

--- backend/students/models.py
+++ backend/students/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Person(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=15, blank=True, null=True)
+    address = models.TextField(blank=True)
+
+    class Meta:
+        abstract = True
