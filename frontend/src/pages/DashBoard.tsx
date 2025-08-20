050e0ec436291c6e04f6fce0adb333a049473089
Bikram <bikikc7@gmail.com>
2025-08-20 12:12:53 +0545

    adding icons in metrix card

--- frontend/src/pages/DashBoard.tsx
+++ frontend/src/pages/DashBoard.tsx
@@ -2,34 +2,51 @@ import React from "react"
 import { useContext } from "react";
 import { SideBarContext } from "../Context/SideBarContext";
 import { CourseChart } from "../components/Metrix/CourseChart";
+import { IoPerson, IoBookSharp } from "react-icons/io5";
+import { FaChalkboardTeacher } from "react-icons/fa";
 export const DashBoard: React.FC = () => {
     const { isOpen } = useContext(SideBarContext);
     return (
         <>
             <div className={` bg-gray-100 w-full p-10 overflow-y-auto flex-1`}>
                 <div className="flex gap-3">
-                    <div className={`bg-blue-600 duration-500 ${isOpen ? `w-85 h-35` : `w-95 h-40`} rounded-2xl grid place-items-center p-8`}>
-                        <h1 className="text-4xl  font-bold">12</h1>
-                        <p className="text-2xl font-bold">
-                            Total Student
-                        </p>
+                    <div
+                        className={`bg-blue-600 duration-500 ${isOpen ? `w-80 h-36` : `w-96 h-40`
+                            } rounded-2xl flex items-center gap-6 p-8`}
+                    >
+                        <IoPerson className="text-white text-6xl" />
+                        <div className="text-center">
+                            <h1 className="text-4xl font-bold text-white">12</h1>
+                            <p className="text-xl font-bold text-white">Total Student</p>
+                        </div>
                     </div>
-                    <div className={`bg-purple-800 duration-500 ${isOpen ? `w-85 h-35` : `w-95 h-40`} rounded-2xl grid place-items-center p-8`}>
-                        <h1 className="text-4xl  font-bold">12</h1>
-                        <p className="text-2xl font-bold">
-                            Total Courses
-                        </p>
+
+                    <div className={`bg-purple-800 duration-500 ${isOpen ? `w-80 h-36` : `w-96 h-40`
+                        } rounded-2xl flex items-center gap-6 p-8`}>
+                        <IoBookSharp className="text-white text-6xl" />
+                        <div className="text-center">
+
+                            <h1 className="text-4xl  font-bold">12</h1>
+                            <p className="text-xl font-bold">
+                                Total Courses
+                            </p>
+                        </div>
                     </div>
-                    <div className={`bg-green-500 duration-500 ${isOpen ? `w-85 h-35` : `w-95 h-40`} rounded-2xl grid place-items-center p-8`}>
-                        <h1 className="text-4xl  font-bold">12</h1>
-                        <p className="text-2xl font-bold">
-                            Total Teachers
-                        </p>
+                    <div className={`bg-green-500 duration-500 ${isOpen ? `w-80 h-36` : `w-96 h-40`
+                        } rounded-2xl flex items-center gap-6 p-8`}>
+                        <FaChalkboardTeacher className="text-white text-6xl" />
+                        <div className="text-center">
+
+                            <h1 className="text-4xl  font-bold">12</h1>
+                            <p className="text-xl font-bold">
+                                Total Teachers
+                            </p>
+                        </div>
                     </div>
                 </div>
                 <div className={`flex gap-20`}>
                     <CourseChart />
-                    <div className={`duration-500 shadow-lg rounded-2xl bg-white  h-100 mt-10  ${isOpen ?`w-70`:`w-80`}`}>
+                    <div className={`duration-500 shadow-lg rounded-2xl bg-white  h-100 mt-10  ${isOpen ? `w-70` : `w-80`}`}>
                         <h1 className="text-center text-2xl m-5 font-bold text-gray-900">Student List</h1>
                         <table className="w-full text-center">
                             <thead className=" text-gray-800">
