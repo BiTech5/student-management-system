e0f1e797a24a3693f27375a153ab2e0cca8ac5cd
Bikram <bikikc7@gmail.com>
2025-08-28 15:25:16 +0545

    feat(Students): add Add Student button and improve layout structure

--- frontend/src/pages/Students.tsx
+++ frontend/src/pages/Students.tsx
@@ -1,8 +1,16 @@
 import { SearchBar } from "../components/Layout/SearchBar";
-const Students=()=>{
-    return(
+const Students = () => {
+    return (
         <>
-        <SearchBar/>
+
+            <section className="flex flex-col w-full" aria-label="Student Management">
+                <SearchBar />
+                <div className="flex justify-end mt-2 mr-3">
+                    <button className="btn px-4 py-2 bg-blue-600 text-white rounded-lg shadow hover:bg-blue-700  hover:scale-110 border-0">
+                        Add Student
+                    </button>
+                </div>
+            </section>
         </>
     )
 }
