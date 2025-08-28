4f5a19076a8758bb4a15fd39a8abd1a2e9d3bae8
Bikram <bikikc7@gmail.com>
2025-08-28 17:33:50 +0545

    feat(StdCard): enhance layout responsiveness and improve text styling based on sidebar state

--- frontend/src/components/Card/StdCard.tsx
+++ frontend/src/components/Card/StdCard.tsx
@@ -1,22 +1,29 @@
+import { useContext } from "react";
+import { SideBarContext } from "../../Context/SideBarContext";
 export const StdCard = () => {
+    const { isOpen } = useContext(SideBarContext);
+
     return (
         <>
-            <div className="flex gap-2 mt-3">
-                <div className={`flex flex-col justify-center items-center text-black border-2 w-65 h-30 border-gray-300 rounded`}>
-                    <h2 className="text-xl font-bold mb-2">Total Students</h2>
-                    <p className="text-5xl font-bold text-blue-600">1245</p>
+            <div className="flex gap-2 mt-4">
+                <div className={`flex flex-col justify-center items-center text-black border-2 duration-500 ${isOpen?'w-57':'w-65 '} h-30 border-gray-300 rounded`}>
+                    <h2 className={`${isOpen?'text-lg':`text-xl`} font-bold duration-500 mb-2`}>Total Students</h2>
+                    <p className={`${isOpen?'text-4xl':'text-5xl'} duration-500 font-bold text-blue-600`}>1245</p>
                 </div>
-                <div className={`flex flex-col justify-center items-center text-black border-2 w-65 h-30 border-gray-300 rounded`}>
-                    <h2 className="text-xl font-bold mb-2">Boys</h2>
-                    <p className="text-5xl font-bold text-red-600">700</p>
+
+                <div className={`flex flex-col justify-center items-center text-black border-2 duration-500 ${isOpen?'w-57':'w-65 '} h-30  border-gray-300 rounded`}>
+                    <h2 className={`${isOpen?'text-lg':`text-xl`} font-bold duration-500 mb-2`}>Boys</h2>
+                    <p className={`${isOpen?'text-4xl':'text-5xl'} duration-500 font-bold text-red-600`}>700</p>
                 </div>
-                <div className={`flex flex-col justify-center items-center text-black border-2 w-65 h-30 border-gray-300 rounded`}>
-                    <h2 className="text-xl font-bold mb-2">Girls</h2>
-                    <p className="text-5xl font-bold text-green-600">545</p>
+
+                <div className={`flex flex-col justify-center items-center text-black border-2 duration-500 ${isOpen?'w-57':'w-65 '} h-30  border-gray-300 rounded`}>
+                    <h2 className={`${isOpen?'text-lg':`text-xl`} font-bold duration-500 mb-2`}>Girls</h2>
+                    <p className={`${isOpen?'text-4xl':'text-5xl'} duration-500 font-bold text-green-600`}>545</p>
                 </div>
-                <div className={`flex flex-col justify-center items-center text-black border-2 w-65 h-30 border-gray-300 rounded`}>
-                    <h2 className="text-xl font-bold mb-2">Classes</h2>
-                    <p className="text-5xl font-bold text-purple-600">12</p>
+
+                <div className={`flex flex-col justify-center items-center text-black border-2 duration-500 ${isOpen?'w-57':'w-65 '} h-30  border-gray-300 rounded`}>
+                    <h2 className={`${isOpen?'text-lg':`text-xl`} font-bold duration-500 mb-2`}>Classes</h2>
+                    <p className={`${isOpen?'text-4xl':'text-5xl'} duration-500 font-bold text-purple-600`}>12</p>
                 </div>
             </div>
         </>
